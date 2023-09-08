a980304270aff803fcc4fb5dafbabccaa6aa0336
RimshaAsad <rimshaasadsimplicore@gmail.com>
2023-09-08 10:30:44 +0500

    csrf token issue fixes

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,9 @@ use Illuminate\Support\Facades\Route;
 //Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 //    return $request->user();
 //});
+Route::get('/csrf-token', function() {
+    return response()->json(['csrf_token' => csrf_token()]);
+});
 
 Route::prefix('company')->controller(UserController::class)->group(function() {
     Route::post('register','register');
